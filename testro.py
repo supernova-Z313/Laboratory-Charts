669cce3ce3793bbfb459ca41303b7c6bd29e1a1f
Supernova <supernova313.313z@gmail.com>
2023-01-05 23:50:50 +0330

    some change to sin

--- testro.py
+++ testro.py
@@ -15,152 +15,153 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # define a list of markevery cases to plot
-cases = [
-    None,
-    8,
-    (30, 8),
-    [16, 24, 32],
-    [0, -1],
-    slice(100, 200, 3),
-    0.1,
-    0.4,
-    (0.2, 0.4)
-]
-
-# data points
-delta = 0.11
-x = np.linspace(0, 10 - 2 * delta, 200) + delta
-y = np.sin(x) + 1.0 + delta
-# plt.plot(x, y, 'o')
-# plt.plot(x, y)
+# cases = [
+#     None,
+#     8,
+#     (30, 8),
+#     [16, 24, 32],
+#     [0, -1],
+#     slice(100, 200, 3),
+#     0.1,
+#     0.4,
+#     (0.2, 0.4)
+# ]
+#
+# # data points
+# delta = 0.11
+# x = np.linspace(0, 10 - 2 * delta, 200) + delta
+# y = np.sin(x) + 1.0 + delta
+# # plt.plot(x, y, 'o')
+# # plt.plot(x, y)
+# # plt.show()
+#
+# fig, axs = plt.subplots(3, 3, figsize=(10, 6), constrained_layout=True)
+# for ax, markevery in zip(axs.flat, cases):
+#     ax.set_title(f'markevery={markevery}')
+#     ax.plot(x, y, 'o', ls='-', ms=4, markevery=markevery)
+#
 # plt.show()
-
-fig, axs = plt.subplots(3, 3, figsize=(10, 6), constrained_layout=True)
-for ax, markevery in zip(axs.flat, cases):
-    ax.set_title(f'markevery={markevery}')
-    ax.plot(x, y, 'o', ls='-', ms=4, markevery=markevery)
-
-plt.show()
 
 # ====================================================================
 
 # fig in plt
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Prepare sample values for variations on y=x graph:
-x = np.linspace(-3, 6, 500)
-fig1 = plt.figure()
-a = fig1.subplots(1, 2, sharex=True)
-print(a)
-ax0, ax1 = a
-
-ax0.plot(x, x)
-ax0.set_yscale('symlog')
-ax0.grid()
-ax0.set_title('symlog')
-
-ax1.plot(x, x)
-ax1.set_yscale('asinh')
-ax1.grid()
-ax1.set_title('asinh')
-
-plt.show()
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# # Prepare sample values for variations on y=x graph:
+# x = np.linspace(-3, 3, 500)  # -3, 6, 500
+# fig1 = plt.figure()
+# a = fig1.subplots(1, 2)  # , sharex=True
+# print(a)
+# ax0, ax1 = a
+#
+# ax0.plot(x, x)
+# ax0.set_yscale('symlog')
+# ax0.grid()
+# ax0.set_title('symlog')
+#
+# x1 = list(range(0, 500))
+# ax1.plot(x1, x)
+# ax1.set_yscale('asinh')
+# ax1.grid()
+# ax1.set_title('asinh')
+#
+# plt.show()
 # =====================================================================
 
 # all scale moudle
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import NullFormatter, FixedLocator
-
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-# make up some data in the interval [0, 1]
-y = np.random.normal(loc=0.5, scale=0.4, size=1000)
-y = y[(y > 0) & (y < 1)]
-y.sort()
-x = np.arange(len(y))
-
-# plot with various axes scales
-fig, axs = plt.subplots(3, 2, figsize=(6, 8),
-                        constrained_layout=True)
-
-# linear
-ax = axs[0, 0]
-ax.plot(x, y)
-ax.set_xlabel("xss")
-ax.set_yscale('linear')
-ax.set_title('linear')
-ax.grid(True)
-
-
-# log
-ax = axs[0, 1]
-ax.plot(x, y)
-ax.set_yscale('log')
-ax.set_title('log')
-ax.grid(True)
-
-
-# symmetric log
-ax = axs[1, 1]
-ax.plot(x, y - y.mean())
-ax.set_yscale('symlog', linthresh=0.02)
-ax.set_title('symlog')
-ax.grid(True)
-
-# logit
-ax = axs[1, 0]
-ax.plot(x, y)
-ax.set_yscale('logit')
-ax.set_title('logit')
-ax.grid(True)
-
-
-# Function x**(1/2)
-def forward(x):
-    return x**(1/2)
-
-
-def inverse(x):
-    return x**2
-
-
-ax = axs[2, 0]
-ax.plot(x, y)
-ax.set_yscale('function', functions=(forward, inverse))
-ax.set_title('function: $x^{1/2}$')
-ax.grid(True)
-ax.yaxis.set_major_locator(FixedLocator(np.arange(0, 1, 0.2)**2))
-ax.yaxis.set_major_locator(FixedLocator(np.arange(0, 1, 0.2)))
-
-
-# Function Mercator transform
-def forward(a):
-    a = np.deg2rad(a)
-    return np.rad2deg(np.log(np.abs(np.tan(a) + 1.0 / np.cos(a))))
-
-
-def inverse(a):
-    a = np.deg2rad(a)
-    return np.rad2deg(np.arctan(np.sinh(a)))
-
-
-ax = axs[2, 1]
-
-t = np.arange(0, 170.0, 0.1)
-s = t / 2.
-
-# ax.plot(t, s, 'o')
-ax.plot(t, s, '-', lw=2)
-
-ax.set_yscale('function', functions=(forward, inverse))
-ax.set_title('function: Mercator')
-ax.grid(True)
-ax.set_xlim([0, 180])
-ax.yaxis.set_minor_formatter(NullFormatter())
-ax.yaxis.set_major_locator(FixedLocator(np.arange(0, 90, 10)))
-
-plt.show()
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from matplotlib.ticker import NullFormatter, FixedLocator
+#
+# # Fixing random state for reproducibility
+# np.random.seed(19680801)
+#
+# # make up some data in the interval [0, 1]
+# y = np.random.normal(loc=0.5, scale=0.4, size=1000)
+# y = y[(y > 0) & (y < 1)]
+# y.sort()
+# x = np.arange(len(y))
+#
+# # plot with various axes scales
+# fig, axs = plt.subplots(3, 2, figsize=(6, 8),
+#                         constrained_layout=True)
+#
+# # linear
+# ax = axs[0, 0]
+# ax.plot(x, y)
+# ax.set_xlabel("xss")
+# ax.set_yscale('linear')
+# ax.set_title('linear')
+# ax.grid(True)
+#
+#
+# # log
+# ax = axs[0, 1]
+# ax.plot(x, y)
+# ax.set_yscale('log')
+# ax.set_title('log')
+# ax.grid(True)
+#
+#
+# # symmetric log
+# ax = axs[1, 1]
+# ax.plot(x, y - y.mean())
+# ax.set_yscale('symlog', linthresh=0.02)
+# ax.set_title('symlog')
+# ax.grid(True)
+#
+# # logit
+# ax = axs[1, 0]
+# ax.plot(x, y)
+# ax.set_yscale('logit')
+# ax.set_title('logit')
+# ax.grid(True)
+#
+#
+# # Function x**(1/2)
+# def forward(x):
+#     return x**(1/2)
+#
+#
+# def inverse(x):
+#     return x**2
+#
+#
+# ax = axs[2, 0]
+# ax.plot(x, y)
+# ax.set_yscale('function', functions=(forward, inverse))
+# ax.set_title('function: $x^{1/2}$')
+# ax.grid(True)
+# ax.yaxis.set_major_locator(FixedLocator(np.arange(0, 1, 0.2)**2))
+# ax.yaxis.set_major_locator(FixedLocator(np.arange(0, 1, 0.2)))
+#
+#
+# # Function Mercator transform
+# def forward(a):
+#     a = np.deg2rad(a)
+#     return np.rad2deg(np.log(np.abs(np.tan(a) + 1.0 / np.cos(a))))
+#
+#
+# def inverse(a):
+#     a = np.deg2rad(a)
+#     return np.rad2deg(np.arctan(np.sinh(a)))
+#
+#
+# ax = axs[2, 1]
+#
+# t = np.arange(0, 170.0, 0.1)
+# s = t / 2.
+#
+# # ax.plot(t, s, 'o')
+# ax.plot(t, s, '-', lw=2)
+#
+# ax.set_yscale('function', functions=(forward, inverse))
+# ax.set_title('function: Mercator')
+# ax.grid(True)
+# ax.set_xlim([0, 180])
+# ax.yaxis.set_minor_formatter(NullFormatter())
+# ax.yaxis.set_major_locator(FixedLocator(np.arange(0, 90, 10)))
+#
+# plt.show()
 
