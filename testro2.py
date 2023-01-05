fe3043a3a5f509ab391b4aa9f8e3a0c3a105fe68
Supernova <supernova313.313z@gmail.com>
2023-01-05 23:51:13 +0330

    sin chart finish

--- testro2.py
+++ testro2.py
@@ -1,27 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-x = [1, 2, 3, 4, 5, 6 , 7, 8]
-y = [10.2, 11.5, 12, 13, 13.9, 14.5, 15, 16.3]
-
-
-# plot with various axes scales
-fig, axs = plt.subplots(1, 2, figsize=(7, 5))
-
-# linear
-ax = axs[0]
-ax.plot(x, y, '-', lw=2)
-ax.plot(x, y)
-ax.set_yscale('linear')
-ax.set_title('linear')
-ax.grid(True)
-
-plt.show()
-
-# x = [1, 2, 3, 4, 5, 6, 7, 8]
-# y = np.sin(x)
-# plt.plot(x, y, "o", ls="-", ms=4)
+#
+# x = [1, 2, 3, 4, 5, 6 , 7, 8]
+# y = [10.2, 11.5, 12, 13, 13.9, 14.5, 15, 16.3]
+#
+#
+# # plot with various axes scales
+# fig, axs = plt.subplots(1, 2, figsize=(7, 5))
+#
+# # linear
+# ax = axs[0]
+# ax.plot(x, y, '-', lw=2)
+# ax.plot(x, y)
+# ax.set_yscale('linear')
+# ax.set_title('linear')
+# ax.grid(True)
+#
 # plt.show()
+# np.pi*2
+fig, axs = plt.subplots(1, 1)
+x = np.linspace(0, 330, 500)
+y = np.sin(x/52.5) + 0.74
+plt.plot(x, y, "o", ls="-", ms=4, markevery=[0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 310, 329])
+# axs.plot(x, y)
+axs.grid()
+axs.set_title("Experiment(8): Graph-2 Voltage changes at different angles.")
+axs.set_xlabel("")
+axs.set_ylabel("V(v)")
+plt.show()
 # ==========================================
 """ for morabaei shape get one state top one but tol har khat , baze v generate them"""
 """
